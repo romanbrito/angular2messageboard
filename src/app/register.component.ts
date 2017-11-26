7ce5d9cc03f9dd30f5836710fb7ec1228c120de7
Roman Brito <romanbrito1@gmail.com>
2017-11-26 10:23:28 -0600

    email validation

--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -18,7 +18,7 @@ export class RegisterComponent {
     this.form = fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      email: ['', Validators.required],
+      email: ['', [Validators.required, emailValid()]],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required]
     }, {validator: matchingFields('password', 'confirmPassword')})
@@ -37,5 +37,13 @@ function matchingFields(field1, field2) {
   return form => {
     if (form.controls[field1].value !== form.controls[field2].value)
       return { mismatchFields: true }
+  }
+}
+
+function emailValid() {
+  return control => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // emailregex.com
+    return regex.test(control.value) ? null : { invalidEmail: true}
   }
 }
