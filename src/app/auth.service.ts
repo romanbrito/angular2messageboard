5e2b201c08b94ff6e917384641b7ed5d680c3553
Roman Brito <romanbrito1@gmail.com>
2017-11-30 09:47:10 -0600

    saving token in local storage in auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
+import {register} from "ts-node/dist";
 
 @Injectable()
 export class AuthService {
 
-  BASE_URL = 'http://localhost:3000/auth';
+  //BASE_URL = 'http://localhost:3000/auth';
+  BASE_URL = 'http://localhost:3000/api';
 
   constructor(private http: Http) {}
 
   register(user) {
-    this.http.post(this.BASE_URL + '/register', user).subscribe();
+    delete user.confirmPassword;
+    this.http.post(this.BASE_URL + '/register', user).subscribe(
+      res => {
+        localStorage.setItem('token', res.json());
+      }
+    );
   }
 }
