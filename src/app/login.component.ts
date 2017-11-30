aadd7718480da03bbdfd5d3fc272c9c4ae558e8f
Roman Brito <romanbrito1@gmail.com>
2017-11-30 15:48:08 -0600

    login form

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -4,7 +4,15 @@ import { AuthService} from "./auth.service";
 @Component({
   selector: 'login',
   template: `
-    
+    <mat-card>
+      <mat-input-container>
+        <input matInput type="email" placeholder="Email">
+      </mat-input-container>
+      <mat-input-container>
+        <input matInput type="password" placeholder="Password">
+      </mat-input-container>
+      <button mat-raised-button color="primary">Login</button>
+    </mat-card>
   `
 })
 export class LoginComponent {
