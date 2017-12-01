c44927a339ecd7911b47d941826d80966127ea57
Roman Brito <romanbrito1@gmail.com>
2017-12-01 10:10:47 -0600

    in auth.service created tokenHeader() to pass the token as a header to authorize request

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Headers, RequestOptions } from "@angular/http";
 import { Router } from "@angular/router";
 
 @Injectable()
@@ -18,6 +18,11 @@ export class AuthService {
 
   get isAuthenticated() {
     return !!localStorage.getItem(this.TOKEN_KEY); // returns true or false
+  }
+
+  get tokenHeader() {
+    const header = new Headers({'Authorization': 'Bearer ' + localStorage.getItem(this.TOKEN_KEY)});
+    return new RequestOptions({headers: header})
   }
 
   login(loginData) {
